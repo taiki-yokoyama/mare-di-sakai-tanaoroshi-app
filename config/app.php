519b1d2f130cfd7cc7e063eb5b8000f7ab36c6ee
taiki-yokoyama <taikig0806yoko@gmail.com>
2026-04-05 11:51:12 +0900

    fix 接続設定

--- config/app.php
+++ config/app.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 return [
     'app_name' => 'mare-di-sakai-tanaoroshi-app',
     'db' => [
-        'host' => 'localhost',
+        'host' => 'sql104.infinityfree.com',
         'port' => 3306,
         'database' => 'mare_di_sakai',
-        'username' => 'root',
-        'password' => '',
+        'username' => 'if0_41555877',
+        'password' => 'LOCCesd5myNo7D',
     ],
     'remember_cookie' => [
         'name' => 'mare_di_sakai_remember',
